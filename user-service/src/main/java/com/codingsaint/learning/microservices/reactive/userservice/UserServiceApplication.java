af1ea7cd0006e440552ee4553c889d4788deaf70
Coding Saint <codingsaint@gmail.com>
2019-09-26 08:38:35 +0530

    User Service

---
+++ user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codingsaint.learning.microservices.reactive.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
