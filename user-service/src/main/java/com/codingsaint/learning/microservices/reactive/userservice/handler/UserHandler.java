215ada343db8c6302418840197601566ce8beef5
Coding Saint <codingsaint@gmail.com>
2019-09-27 19:38:36 +0530

    Adding User Handler to user-service

---
+++ user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/handler/UserHandler.java
@@ -0,0 +1,50 @@
+package com.codingsaint.learning.microservices.reactive.userservice.handler;
+
+import com.codingsaint.learning.microservices.reactive.userservice.model.User;
+import com.codingsaint.learning.microservices.reactive.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserHandler {
+    private final UserRepository userRepository;
+
+    public Mono<ServerResponse> getAll(ServerRequest serverRequest) {
+        Mono<User> user = serverRequest.bodyToMono(User.class);
+        return ServerResponse.ok()
+                .body(BodyInserters.fromPublisher(
+                        userRepository.findAll(), User.class));
+    }
+
+    public Mono<ServerResponse> add(ServerRequest serverRequest) {
+        Mono<User> user = serverRequest.bodyToMono(User.class);
+        return ServerResponse.status(HttpStatus.OK)
+                .body(BodyInserters.fromPublisher(
+                        user.flatMap(userRepository::save), User.class));
+    }
+
+    public Mono<ServerResponse> update(ServerRequest serverRequest) {
+        Mono<User> user = serverRequest.bodyToMono(User.class);
+        return ServerResponse.status(HttpStatus.OK)
+                .body(BodyInserters.fromPublisher(
+                        user.flatMap(userRepository::save), User.class));
+    }
+
+    public Mono<ServerResponse> delete(ServerRequest serverRequest) {
+        String _id = serverRequest.pathVariable("id");
+        return ServerResponse.noContent().build(userRepository.deleteById(_id));
+    }
+
+    public Mono<ServerResponse> get(ServerRequest serverRequest) {
+        String _id = serverRequest.pathVariable("id");
+        return ServerResponse.ok()
+                .body(BodyInserters.fromPublisher(
+                        userRepository.findById(_id), User.class));
+    }
+}
---
+++ user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/routes/UserRouteConfiguration.java
@@ -0,0 +1,30 @@
+package com.codingsaint.learning.microservices.reactive.userservice.routes;
+
+import com.codingsaint.learning.microservices.reactive.userservice.handler.UserHandler;
+import com.codingsaint.learning.microservices.reactive.userservice.model.User;
+import com.codingsaint.learning.microservices.reactive.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@RequiredArgsConstructor
+public class UserRouteConfiguration {
+    private final UserRepository userRepository;
+    private final UserHandler userHandler;
+
+    @Bean
+    public RouterFunction userRoutes() {
+        return RouterFunctions.route(
+                RequestPredicates.GET("/users"), userHandler::getAll)
+                .andRoute(RequestPredicates.POST("/user"), userHandler::add)
+                .andRoute(RequestPredicates.PUT("/user"), userHandler::update)
+                .andRoute(RequestPredicates.GET("/user/{id}"), userHandler::get)
+                .andRoute(RequestPredicates.DELETE("/user/{id}"), userHandler::delete);
+    }
+
+}
