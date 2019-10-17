d4d60644a064a9f4ed3bc11f1bbca3143b2676f1
Coding Saint <codingsaint@gmail.com>
2019-10-17 23:38:16 +0530

    Moved Endpoint user tasks to Handler

--- user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/handler/UserHandler.java
+++ user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/handler/UserHandler.java
@@ -3,17 +3,28 @@ package com.codingsaint.learning.microservices.reactive.userservice.handler;
 import com.codingsaint.learning.microservices.reactive.userservice.model.User;
 import com.codingsaint.learning.microservices.reactive.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserHandler {
     private final UserRepository userRepository;
+    private final WebClient webClient;
+    private final DiscoveryClient discoveryClient;
+
+
 
     public Mono<ServerResponse> getAll(ServerRequest serverRequest) {
         Mono<User> user = serverRequest.bodyToMono(User.class);
@@ -24,6 +35,7 @@ public class UserHandler {
 
     public Mono<ServerResponse> add(ServerRequest serverRequest) {
         Mono<User> user = serverRequest.bodyToMono(User.class);
+
         return ServerResponse.status(HttpStatus.CREATED)
                 .body(BodyInserters.fromPublisher(
                         user.flatMap(userRepository::save), User.class));
@@ -46,5 +58,20 @@ public class UserHandler {
         return ServerResponse.ok()
                 .body(BodyInserters.fromPublisher(
                         userRepository.findById(_id), User.class));
+    }
+    public Mono<ServerResponse> userTasks(ServerRequest serverRequest){
+        System.out.println(serverRequest.pathVariable("id"));
+        List<ServiceInstance> instances = discoveryClient.getInstances("todo-service");
+        ServiceInstance instance = instances.stream().findAny()
+                .orElseThrow(() -> new IllegalStateException("No proxy instance available"));
+        return  webClient.get()
+                .uri( instance.getUri().toString()+"/todos/user/"+serverRequest.pathVariable("id"))
+                .exchange().flatMap((ClientResponse clientResponse)->{
+                    return ServerResponse.status(clientResponse.statusCode())
+                            .headers(c -> clientResponse.headers().asHttpHeaders().forEach((name, value) ->
+                                    c.put(name, value)))
+                            .body(clientResponse.bodyToFlux(DataBuffer.class), DataBuffer.class);
+                });
+
     }
 }
--- user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/routes/UserRouteConfiguration.java
+++ user-service/src/main/java/com/codingsaint/learning/microservices/reactive/userservice/routes/UserRouteConfiguration.java
@@ -1,31 +1,19 @@
 package com.codingsaint.learning.microservices.reactive.userservice.routes;
 
 import com.codingsaint.learning.microservices.reactive.userservice.handler.UserHandler;
-import com.codingsaint.learning.microservices.reactive.userservice.model.User;
 import com.codingsaint.learning.microservices.reactive.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.server.*;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 
 @Configuration
 @RequiredArgsConstructor
 public class UserRouteConfiguration {
     private final UserRepository userRepository;
     private final UserHandler userHandler;
-    private final WebClient webClient;
-
-    private final DiscoveryClient discoveryClient;
 
     @Bean
     public RouterFunction userRoutes() {
@@ -35,22 +23,7 @@ public class UserRouteConfiguration {
                 .andRoute(RequestPredicates.PUT("/user"), userHandler::update)
                 .andRoute(RequestPredicates.GET("/user/{id}"), userHandler::get)
                 .andRoute(RequestPredicates.DELETE("/user/{id}"), userHandler::delete)
-                .andRoute(RequestPredicates.GET("/user/{id}/todos"),serverRequest -> {
-                    System.out.println(serverRequest.pathVariable("id"));
-                    List<ServiceInstance> instances = discoveryClient.getInstances("todo-service");
-                    ServiceInstance instance = instances.stream().findAny()
-                            .orElseThrow(() -> new IllegalStateException("No proxy instance available"));
-                    System.out.println(instance.getUri().toString());
+                .andRoute(RequestPredicates.GET("/user/{id}/todos"), userHandler::userTasks);
 
-                    return  webClient.get()
-                            .uri( instance.getUri().toString()+"/todos/user/"+serverRequest.pathVariable("id"))
-                            .exchange().flatMap((ClientResponse clientResponse)->{
-                        return ServerResponse.status(clientResponse.statusCode())
-                                .headers(c -> clientResponse.headers().asHttpHeaders().forEach((name, value) ->
-                                        c.put(name, value)))
-                                .body(clientResponse.bodyToFlux(DataBuffer.class), DataBuffer.class);
-                    });
-                });
     }
-
 }
